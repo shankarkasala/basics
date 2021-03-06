b2574b0c99049df2a5ddebabbbc47d433f6e8946
shankarkasala <80156261+shankarkasala@users.noreply.github.com>
2021-03-06 17:25:09 +0530

    Update Helloworld.js

--- Helloworld/src/Helloworld.js
+++ Helloworld/src/Helloworld.js
@@ -1,26 +1,13 @@
 import React from 'react'
-import {useSpring, animated} from 'react-spring'
-const heading={
+const Helloworld=()=>{
+  const heading={
     fontSize:'30px',
     color:'blue'
-}
-const Helloworld=()=>{
-    const  props = useSpring({
-        opacity: 1,
-        from: { opacity: 0 },
-       config:{  duration: 1000  },
-       color:'blue'
-      })
-      const props1 = useSpring({ number: 1, from: { number: 0 }, config:{  duration: 1000 }, })
+} 
 return(
     <div>
-        <h1 style={heading}>Hello React</h1>
-        <animated.h1 style={props}>hello</animated.h1>
-        <animated.span>{props1.number}</animated.span>
+        <h1 style={heading}>Hello React</h1>       
     </div>
-    
 )
-
-
 }
 export default Helloworld

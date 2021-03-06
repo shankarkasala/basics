ec863877f500a8bed71816fc6b5f8292ff15dce4
shankarkasala <80156261+shankarkasala@users.noreply.github.com>
2021-03-06 17:16:14 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Helloworld from './components/Helloworld';
+
+function App() {
+
+  return (
+    <div className="App">
+    <Helloworld/>
+    </div>
+  );
+}
+
+export default App;
